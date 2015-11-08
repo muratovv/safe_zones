298b1acfa903acc639f77a3cbd5a43e462e53854
Muratov Fyodor <muratovfyodor@yandex.ru>
2015-11-08 10:28:19 +0300

    Added AdapterUtil class

---
+++ src/main/java/HyperEdgeFramework/AdapterUtil.java
@@ -0,0 +1,23 @@
+package HyperEdgeFramework;
+
+import com.github.davidmoten.rtree.geometry.Circle;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+public class AdapterUtil
+{
+	public static Polygon polygon(GeometryFactory gFactory, Circle circle, int splits)
+	{
+		double radius = radius(circle);
+		Point point = gFactory.createPoint(new Coordinate(circle.x(), circle.y()));
+		return ((Polygon) point.buffer(radius, splits));
+	}
+
+	private static double radius(Circle circle)
+	{
+		double a = circle.mbr().perimeter() / 4.;
+		return a / 2.;
+	}
+}
