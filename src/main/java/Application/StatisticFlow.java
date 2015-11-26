24649d4befddcc7f79acf42d85594c4ca6e3613d
Muratov Fyodor <muratovfyodor@yandex.ru>
2015-11-26 23:48:18 +0300

    dataset generator move to grid

--- src/main/java/Application/StatisticFlow.java
+++ src/main/java/Application/StatisticFlow.java
@@ -4,15 +4,15 @@ import Application.Statistic.ComputedGraphStat;
 import Application.Statistic.ShortestPathStat;
 import Application.Statistic.SourceDataSetStat;
 import Application.Statistic.WholeStatistic;
-import HyperEdgeFramework.Grid;
 import HyperEdgeFramework.HyperEdgeFlow.Algorithm;
 import HyperEdgeFramework.HyperEdgeFlow.ComputedGraphDistance;
 import HyperEdgeFramework.HyperEdgeFlow.Inserter;
 import HyperEdgeFramework.Inflater;
 import HyperEdgeFramework.PreferredZone;
 import HyperEdgeFramework.Util.GeomUtil;
-import com.github.davidmoten.rtree.geometry.Circle;
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 import javafx.util.Pair;
 import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.SimpleWeightedGraph;
@@ -21,8 +21,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.stream.Collectors;
+
+import static HyperEdgeFramework.Grid.DataSetGenerator.generateEasyRandomDataSet;
 
 public class StatisticFlow
 {
@@ -52,72 +53,11 @@ public class StatisticFlow
 	{
 		double alpha = 0.1;
 		GeomUtil.Metric metric = new GeomUtil.Metric.Euclidean();
-		ArrayList<Circle> circles = Grid.squareGrid(20, com.github.davidmoten.rtree.geometry.Point.create(0, 0), 2, 2);
-		ArrayList<Polygon> polygons = Inflater.map(circles, 3);
-		Pair<Coordinate, Coordinate> startEnd = new Pair<>(new Coordinate(-10, 0), new Coordinate(500, -500));
-//
-		ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> datasets = new ArrayList<>();
-		ArrayList<Pair<Coordinate, Coordinate>> points = new ArrayList<>();
-		points.add(startEnd);
-		datasets.add(new Pair<>(polygons, points));
-		datasets = generateDataSet();
+		ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> datasets =
+				generateEasyRandomDataSet(1, 1, 2, 5, 3);
 
 		StatisticFlow statisticFlow = new StatisticFlow(new File("test.txt"), datasets, alpha, metric);
 		statisticFlow.runAll();
-	}
-
-	public static ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> generateDataSet()
-	{
-		int datasetSize = 10;
-		int pathSize = 10;
-
-		int polySplit = 10;
-
-		ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> dataSets = new ArrayList<>();
-		for (int i = 1; i <= datasetSize; i++)
-		{
-			ArrayList<Polygon> polygons = Inflater.map(Grid.squareGrid(i + 5, com.github.davidmoten.rtree.geometry.Point.create(0, 0), i * 2, i + 3), polySplit);
-			MultiPolygon multiPolygon = GeomUtil.factory().createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
-
-			ArrayList<Pair<Coordinate, Coordinate>> paths = new ArrayList<>();
-			for (int j = 0; j < pathSize; j++)
-			{
-				Point start = generatePoint(multiPolygon, polygons);
-				Point end = generatePoint(multiPolygon, polygons);
-				paths.add(new Pair<>(start.getCoordinate(), end.getCoordinate()));
-			}
-			dataSets.add(new Pair<>(polygons, paths));
-		}
-		return dataSets;
-	}
-
-	private static Point generatePoint(MultiPolygon stat, ArrayList<Polygon> polygons)
-	{
-		Random random = new Random();
-		Geometry envelope = stat.getEnvelope();
-
-		int minX = 0, minY = 0;
-		int maxX = 0, maxY = 0;
-		for (Coordinate coordinate : envelope.getCoordinates())
-		{
-			if (minX > coordinate.x)
-				minX = ((int) coordinate.x);
-			if (minY > coordinate.y)
-				minY = ((int) coordinate.y);
-			if (maxX < coordinate.x)
-				maxX = ((int) coordinate.x);
-			if (maxY < coordinate.y)
-				maxY = ((int) coordinate.y);
-		}
-
-		Point point = null;
-		do
-		{
-			int xRand = random.nextInt((maxX - minX) + 1) + minX;
-			int yRand = random.nextInt((maxY - minY) + 1) + minY;
-			point = GeomUtil.factory().createPoint(new Coordinate(xRand, yRand));
-		} while (Inserter.inAnyZone(polygons, point));
-		return point;
 	}
 
 	public void runAll() throws IOException
@@ -146,8 +86,6 @@ public class StatisticFlow
 		SourceDataSetStat dataSetStat = new SourceDataSetStat(zones);
 
 		long end = System.nanoTime();
-//		System.out.print(writer.write(dataSetStat.toString(), 0));
-//		System.out.print(writer.write(String.format(", load on %d\n", TimeUnit.NANOSECONDS.toSeconds(end - start)), 0));
 
 		wholeStatistic.setDataSetStat(dataSetStat, end - start);
 
@@ -159,8 +97,6 @@ public class StatisticFlow
 
 		ComputedGraphStat graphStat = new ComputedGraphStat(G);
 
-//		System.out.print(writer.write(graphStat.toString(), 1));
-//		System.out.print(writer.write(String.format(", computed on %d\n", TimeUnit.NANOSECONDS.toSeconds(end - start)), 0));
 
 		wholeStatistic.setGraphStat(graphStat, end - start);
 		System.out.println(wholeStatistic.generateGraphStat());
@@ -174,20 +110,14 @@ public class StatisticFlow
 
 			long insertTime = end - start;
 
-//			System.out.print(writer.write(String.format("points added on %d\n", TimeUnit.NANOSECONDS.toSeconds(end - start)), 2));
-
 
 			start = System.nanoTime();
 			DijkstraShortestPath<Integer, Algorithm.Edge> shortestPath = shortestPath(G);
-
-//			System.out.println("shortest path size " + shortestPath.getPathEdgeList().size());
 
 			double distance = ComputedGraphDistance.compute(shortestPath.getPathEdgeList(), inflater.getNotVisited());
 			end = System.nanoTime();
 
 			ShortestPathStat stat = new ShortestPathStat(shortestPath, distance);
-//			System.out.print(writer.write(String.format("%s, create on %d\n", stat, TimeUnit.NANOSECONDS.toSeconds(end - start)), 3));
-
 			long shortestPathTime = end - start;
 
 			wholeStatistic.append(stat, insertTime + shortestPathTime);
@@ -216,9 +146,9 @@ public class StatisticFlow
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
-	private ArrayList<Pair<Point, Point>> generatePoints(ArrayList<Pair<Coordinate, Coordinate>> rowCoordinatates)
+	private ArrayList<Pair<Point, Point>> generatePoints(ArrayList<Pair<Coordinate, Coordinate>> rowCoordinates)
 	{
-		return rowCoordinatates.stream().map(coordPair -> new Pair<>(GeomUtil.factory().createPoint(coordPair.getKey()),
+		return rowCoordinates.stream().map(coordPair -> new Pair<>(GeomUtil.factory().createPoint(coordPair.getKey()),
 				GeomUtil.factory().createPoint(coordPair.getValue()))).collect(Collectors.toCollection(ArrayList<Pair<Point, Point>>::new));
 	}
 
--- src/main/java/HyperEdgeFramework/Grid.java
+++ src/main/java/HyperEdgeFramework/Grid.java
@@ -1,9 +1,17 @@
 package HyperEdgeFramework;
 
+import HyperEdgeFramework.HyperEdgeFlow.Inserter;
+import HyperEdgeFramework.Util.GeomUtil;
 import com.github.davidmoten.rtree.geometry.Circle;
 import com.github.davidmoten.rtree.geometry.Point;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Grid
 {
@@ -29,5 +37,70 @@ public class Grid
 			square.addAll(linearGrid(line, Point.create(x, y), rad, between));
 		}
 		return square;
+	}
+
+	public static class DataSetGenerator
+	{
+		private static Random random = new Random();
+
+		public static ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>>
+		generateEasyRandomDataSet(int qOfDatasets, int qOfPathFinding, int fromQOfZone, int toQOfZones, int polySplit)
+		{
+
+			ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> dataSets = new ArrayList<>();
+
+			for (int i = 1; i <= qOfDatasets + 1; i++)
+			{
+				ArrayList<Polygon> polygons = Inflater.map(squareGrid(toQOfZones, Point.create(0, 0), i, i * 2 + 11), polySplit);
+				int zonesForRemove = Math.abs(random.nextInt((toQOfZones - fromQOfZone) + 1));
+
+				for (int j = 0; j < zonesForRemove; j++)
+				{
+					polygons.remove(Math.abs(random.nextInt(polygons.size())));
+				}
+
+				MultiPolygon multiPolygon = GeomUtil.factory().createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
+
+
+				ArrayList<Pair<Coordinate, Coordinate>> paths = new ArrayList<>();
+				for (int j = 0; j < qOfPathFinding; j++)
+				{
+					com.vividsolutions.jts.geom.Point start = generatePoint(multiPolygon, polygons);
+					com.vividsolutions.jts.geom.Point end = generatePoint(multiPolygon, polygons);
+					paths.add(new Pair<>(start.getCoordinate(), end.getCoordinate()));
+				}
+				dataSets.add(new Pair<>(polygons, paths));
+			}
+			return dataSets;
+		}
+
+		public static com.vividsolutions.jts.geom.Point generatePoint(MultiPolygon stat, ArrayList<Polygon> polygons)
+		{
+			Random random = new Random();
+			Geometry envelope = stat.getEnvelope();
+
+			int minX = 0, minY = 0;
+			int maxX = 0, maxY = 0;
+			for (Coordinate coordinate : envelope.getCoordinates())
+			{
+				if (minX > coordinate.x)
+					minX = ((int) coordinate.x);
+				if (minY > coordinate.y)
+					minY = ((int) coordinate.y);
+				if (maxX < coordinate.x)
+					maxX = ((int) coordinate.x);
+				if (maxY < coordinate.y)
+					maxY = ((int) coordinate.y);
+			}
+
+			com.vividsolutions.jts.geom.Point point;
+			do
+			{
+				int xRand = random.nextInt((maxX - minX) + 1) + minX;
+				int yRand = random.nextInt((maxY - minY) + 1) + minY;
+				point = GeomUtil.factory().createPoint(new Coordinate(xRand, yRand));
+			} while (Inserter.inAnyZone(polygons, point));
+			return point;
+		}
 	}
 }
