1d8daf1b84fbe33e052962bdf2e3e7da8afd37f0
Muratov Fyodor <muratovfyodor@yandex.ru>
2015-12-04 16:57:33 +0300

    refactor grid

--- src/main/java/HyperEdgeFramework/Grid.java
+++ src/main/java/HyperEdgeFramework/Grid.java
@@ -44,32 +44,16 @@ public class Grid
 		private static Random random = new Random();
 
 		public static ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>>
-		generateEasyRandomDataSet(int qOfDatasets, int qOfPathFinding, int fromQOfZone, int toQOfZones, int polySplit)
+		generateEasyRandomDataSets(int qOfDatasets, int qOfPathFinding, int fromQOfZone, int toQOfZones, int polySplit)
 		{
 
 			ArrayList<Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>>> dataSets = new ArrayList<>();
 
 			for (int i = 1; i <= qOfDatasets + 1; i++)
 			{
-				ArrayList<Polygon> polygons = Inflater.map(squareGrid(toQOfZones, Point.create(0, 0), i, i * 2 + 11), polySplit);
-				int zonesForRemove = Math.abs(random.nextInt((toQOfZones - fromQOfZone) + 1));
-
-				for (int j = 0; j < zonesForRemove; j++)
-				{
-					polygons.remove(Math.abs(random.nextInt(polygons.size())));
-				}
-
-				MultiPolygon multiPolygon = GeomUtil.factory().createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
-
-
-				ArrayList<Pair<Coordinate, Coordinate>> paths = new ArrayList<>();
-				for (int j = 0; j < qOfPathFinding; j++)
-				{
-					com.vividsolutions.jts.geom.Point start = generatePoint(multiPolygon, polygons);
-					com.vividsolutions.jts.geom.Point end = generatePoint(multiPolygon, polygons);
-					paths.add(new Pair<>(start.getCoordinate(), end.getCoordinate()));
-				}
-				dataSets.add(new Pair<>(polygons, paths));
+				EasyRandomDataset easyRandomDataset = new EasyRandomDataset(qOfPathFinding, fromQOfZone, toQOfZones,
+						polySplit, i, i * 2 + 11);
+				dataSets.add(easyRandomDataset.generate());
 			}
 			return dataSets;
 		}
@@ -101,6 +85,67 @@ public class Grid
 				point = GeomUtil.factory().createPoint(new Coordinate(xRand, yRand));
 			} while (Inserter.inAnyZone(polygons, point));
 			return point;
+		}
+
+		public static class EasyRandomDataset
+		{
+			private int qOfPathFinding;
+			private int fromQOfZone;
+			private int toQOfZones;
+			private int polySplit;
+			private double radius;
+			private double distanse;
+			private ArrayList<Polygon> polygons;
+			private ArrayList<Pair<Coordinate, Coordinate>> paths;
+
+			public EasyRandomDataset(int qOfPathFinding, int fromQOfZone, int toQOfZones, int polySplit, double radius, double distance)
+			{
+				this.qOfPathFinding = qOfPathFinding;
+				this.fromQOfZone = fromQOfZone;
+				this.toQOfZones = toQOfZones;
+				this.polySplit = polySplit;
+				this.radius = radius;
+				this.distanse = distance;
+				invoke();
+			}
+
+			public ArrayList<Polygon> getPolygons()
+			{
+				return polygons;
+			}
+
+			public ArrayList<Pair<Coordinate, Coordinate>> getPaths()
+			{
+				return paths;
+			}
+
+			private EasyRandomDataset invoke()
+			{
+				polygons = Inflater.map(squareGrid(toQOfZones, Point.create(0, 0), radius, distanse), polySplit);
+				int zonesForRemove = Math.abs(random.nextInt((toQOfZones - fromQOfZone) + 1));
+
+				for (int j = 0; j < zonesForRemove; j++)
+				{
+					polygons.remove(Math.abs(random.nextInt(polygons.size())));
+				}
+
+				MultiPolygon multiPolygon = GeomUtil.factory().createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
+
+
+				paths = new ArrayList<>();
+				for (int j = 0; j < qOfPathFinding; j++)
+				{
+					com.vividsolutions.jts.geom.Point start = generatePoint(multiPolygon, polygons);
+					com.vividsolutions.jts.geom.Point end = generatePoint(multiPolygon, polygons);
+					paths.add(new Pair<>(start.getCoordinate(), end.getCoordinate()));
+				}
+				return this;
+			}
+
+			public Pair<ArrayList<Polygon>, ArrayList<Pair<Coordinate, Coordinate>>> generate()
+			{
+				return new Pair<>(getPolygons(), getPaths());
+			}
 		}
 	}
 }
